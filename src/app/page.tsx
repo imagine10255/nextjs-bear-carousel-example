54950adf204ca66217754e9a09be6eaae544cfbc
imagine10255 <imagine10255@gmail.com>
2023-06-26 19:10:33 +0800

    add bear-react-carousel library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import Image from 'next/image'
+import "bear-react-carousel/dist/index.css";
+import Carousel from '@/components/Carousel';
 
 export default function Home() {
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -24,19 +27,22 @@ export default function Home() {
               height={24}
               priority
             />
+
           </a>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div style={{width: '100%'}} className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
+        {/*<Image*/}
+        {/*  className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"*/}
+        {/*  src="/next.svg"*/}
+        {/*  alt="Next.js Logo"*/}
+        {/*  width={180}*/}
+        {/*  height={37}*/}
+        {/*  priority*/}
+        {/*/>*/}
+        <Carousel/>
+
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
@@ -106,6 +112,7 @@ export default function Home() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
+
         </a>
       </div>
     </main>
---
+++ src/components/Carousel.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import BearCarousel, {BearSlideCard} from 'bear-react-carousel';
+
+
+export default function Carousel() {
+
+    const images = [
+        { id: 1, imageUrl: "https://dummyimage.com/900x400/dee2e6/6c757d.jpg" },
+        { id: 2, imageUrl: "https://dummyimage.com/900x400/dee2e6/6c757d.jpg" },
+        { id: 3, imageUrl: "https://dummyimage.com/900x400/dee2e6/6c757d.jpg" }
+    ];
+
+    const bearSlideItemData = images.map((row) => {
+        return {
+            key: row.id,
+            children: <BearSlideCard bgUrl={row.imageUrl} />
+        };
+    });
+
+    return <BearCarousel data={bearSlideItemData} height="400px" />;
+
+}
